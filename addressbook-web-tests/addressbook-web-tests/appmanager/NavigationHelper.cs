2f8362364a1d2fab8c2ae61e4742db56231eb8ae
assemblyad <61529304+assemblyad@users.noreply.github.com>
2021-09-18 20:30:11 -0600

    Лекция 3.4. Оптимизация переходов между страницами

--- addressbook-web-tests/addressbook-web-tests/appmanager/NavigationHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/NavigationHelper.cs
@@ -17,6 +17,10 @@ namespace WebAdressbookTests
         }
         public void GoToHomePage()
         {
+            if (driver.Url == baseURL + "/addressbook/")
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL + "/addressbook/");
         }
         public void GoToContactHomePage()
@@ -25,6 +29,10 @@ namespace WebAdressbookTests
         }
         public void GoToGroupsPage()
         {
+            if (driver.Url == baseURL +"/addressbook/group.php" && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("groups")).Click();
         }
     }
