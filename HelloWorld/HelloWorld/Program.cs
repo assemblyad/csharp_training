f812d00977a7d39e36ed161b2ba7dfcc4ddc09d1
assemblyad <61529304+assemblyad@users.noreply.github.com>
2021-09-21 20:01:53 -0600

    mt test add solution

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //int someNumber;
+
+        static void Main(string[] args)
+        {
+
+            //variables
+            //string file = @"C:\my documents\homedirectory\files";
+            string file = "C:\\my documents\n homedirectory\\files";
+            Console.WriteLine(file);
+
+            /*
+            string itemName = "Widget";
+            double pricePerOunce = 17.36;
+            string outputString;
+
+            outputString = String.Format("{0,10},{1,10}", itemName, pricePerOunce);
+            //outputString = String.Format("The currebt price is {0:c}" , pricePerOunce);
+
+
+            Console.WriteLine(String.Format("{0,10},{1,10}","Item name", "Price"));
+            Console.WriteLine(outputString);
+
+
+
+            //format output
+            //outputString = "The current price is " + pricePerOunce;
+
+
+            
+            string userFirstName;
+            string age;
+
+            Console.WriteLine("What is your fisrt name");
+            userFirstName = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            age = Console.ReadLine();
+
+            //string responce = "Your name is: " + userFirstName + " and you are " + age + "years old";
+            string responce = $"Your name is:  {userFirstName} and you are {age} years old";
+
+            Console.WriteLine(responce);
+           
+           Console.WriteLine("what is yuor favorite number?");
+           int number;
+           number = Convert.ToInt32(Console.ReadLine());
+
+           int number = 5;
+           int sum;
+           sum = number + 5;
+
+           bool test = true;
+           sum = number + test;
+
+
+           int number1=0;
+           char holdsALetter = 'a';
+           double amount;
+           amount = 2
+               sdsd = 545;
+
+           asdasd
+               sdfsdf
+               aas
+
+               34985734895j
+               sdfklsdkl
+           */
+
+        }
+    }
+}
