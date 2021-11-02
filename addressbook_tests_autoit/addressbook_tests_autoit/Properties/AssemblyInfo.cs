25eb24d90adc759b294b4a3b300f2f043bae4f37
assemblyad <61529304+assemblyad@users.noreply.github.com>
2021-11-01 18:56:54 -0600

    # question 1 Лекция 8.1. Автоматизация десктопных приложений при помощи AutoItX

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a540cb21-f75a-47c8-a018-4e65851ffa9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class UnitTest1: TestBase
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
