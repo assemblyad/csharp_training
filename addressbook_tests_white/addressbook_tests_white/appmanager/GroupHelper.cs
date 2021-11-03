8d6140d20ffaab391173fb1e34fae852604dffd9
assemblyad <61529304+assemblyad@users.noreply.github.com>
2021-11-02 20:02:54 -0600

    Лекция 8.2. Автоматизация десктопных приложений при помощи White -- In progress

---
+++ addressbook_tests_white/addressbook_tests_white/appmanager/GroupHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace addressbook_tests_white
+{
+    public class GroupHelper: HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager): base(manager) { }
+
+        public List<GroupData> GetGroupsList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("");
+
+            TreeNode root = tree.Nodes[0];
+
+            foreach (TreeNode item in root.Nodes)
+            {
+                list.Add(new GroupData()
+                {
+                    Name = item.Text
+
+                });
+            }
+            CloseGroupsDialogue(dialogue);
+            return new List<GroupData>();
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            Window dialogue  = OpenGroupsDialogue();
+            dialogue.Get<Button>("").Click();
+
+            
+            //aux.Send(newGroup.Name);
+            //aux.Send("{ENTER}");
+            CloseGroupsDialogue(dialogue);    
+        }
+
+        private void CloseGroupsDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("").Click();
+            
+        }
+
+        private Window OpenGroupsDialogue()
+        {
+            manager.MainWindow.Get<Button>("").Click();
+            return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+    }
+}
